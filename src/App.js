c8d9f92d9359151c6a2d81b54a7024ce70e2f2cc
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2019-02-10 02:10:34 +0200

    Working frontend for POC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CourseColumn from './CourseColumn';
 
 class App extends Component {
+  state = {
+    prequisites: [
+      { courseName: "Linis 1",
+        topics: [
+          { name: 'Käänteismatriisi', value: 1 },
+          { name: 'Matriisin transpoosi', value: 1 },
+          { name: 'Matriisin determinantti', value: 1 }
+        ]
+      },
+      { courseName: "Linis 2",
+        topics: [
+          { name: 'Matriisin derivaatta', value: 1 },
+          { name: 'Matriisin integrointi', value: 1 },
+          { name: 'Vectoreiden yhteenlasku', value: 1 },
+          { name: 'Matriisin nypläys', value: 1 }
+        ]
+      },
+      { courseName: "Todennäköisyyslaskenta I",
+        topics: [
+          { name: 'Todennäköisyyksien laskusäännöt', value: 1 },
+          { name: 'Bayesin kaava', value: 1 },
+          { name: 'Diskreetit todennäköisyydet', value: 1 }
+        ]
+      }  
+    ],
+    targetCourse: {
+      courseName: 'IML',
+      topics: [
+        { name: 'clustering', value: 1 },
+        { name: 'overfit', value: 1 },
+        { name: 'cross-validation', value: 1 }
+      ]
+    }
+  }
+
+  saveResults = () => {
+    console.log(JSON.stringify(this.state))
+  }
+
+  updateTopicValue = (courseName, topicText, value) => {
+    const p = this.state.prequisites
+
+    const courseIndex = p
+      .findIndex(course => course.courseName === courseName);
+    const topicIndex = p[courseIndex].topics
+      .findIndex(topic => topic.name === topicText)
+    
+    const updatedTopic = { ...p[courseIndex].topics[topicIndex], value: value };
+
+    const updatedTopics = [
+      ...p[courseIndex].topics.slice(0, topicIndex),
+      updatedTopic,
+      ...p[courseIndex].topics.slice(topicIndex + 1),
+    ];
+
+    const updatedCourse = { ...p[courseIndex], topics: updatedTopics }
+
+    const updatedPrequisites = [
+      ...p.slice(0, courseIndex),
+      updatedCourse,
+      ...p.slice(courseIndex + 1)
+    ]
+
+    this.setState({ ...this.state, prequisites: updatedPrequisites })
+  }
+
+  updateTargetTopicValue = (courseName, topicText, value) => {
+    const t = this.state.targetCourse
+    const topicIndex = t.topics
+      .findIndex(topic => topic.name === topicText)
+    const updatedTopic = { ...t.topics[topicIndex], value: value }
+    const updatedTopics = [
+      ...t.topics.slice(0, topicIndex),
+      updatedTopic,
+      ...t.topics.slice(topicIndex + 1),
+    ];
+    const updatedCourse = { ...t, topics: updatedTopics }
+    this.setState({ ...this.state, targetCourse: updatedCourse })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div className="curri-column-container">
+          {this.state.prequisites.map(course => 
+            <CourseColumn
+              key={course.courseName}
+              course={course.courseName}
+              topics={course.topics}
+              updateTopicValueF={(courseName, topicText, value) => this.updateTopicValue(courseName, topicText, value)}
+            />
+          )}
+        </div>
+        
+        <div className="right-menu">
+          <div className="right-menu-splitter">
+            <CourseColumn
+              course={this.state.targetCourse.courseName}
+              topics={this.state.targetCourse.topics}
+              updateTopicValueF={(courseName, topicText, value) => this.updateTargetTopicValue(courseName, topicText, value)}
+            />
+            <button 
+              className="curri-button send"
+              style={{backgroundColor: '#e8e8e8'}}
+              onClick={this.saveResults}>
+              Save
+            </button>
+          </div>
+        </div>
+
       </div>
     );
   }
---
+++ src/ToggleButton.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class ToggleButton extends Component {
+  state = {
+    toggleSetting: 1
+  };
+
+  increment = (i) => {
+    return i === 3 ? 1 : i + 1
+  }
+
+  
+  handleClick = () => {
+    this.props.updateTopicValue(this.props.course, this.props.topic.name, this.increment(this.props.topic.value))
+    this.setState(({ toggleSetting }) => ({
+      toggleSetting: this.increment(toggleSetting) 
+    }));
+  };
+
+  buttonStyle = (toggleValue) => {
+    switch (toggleValue) {
+      case 1:
+        return {backgroundColor: '#ffffff'}
+      case 2:
+        return {backgroundColor: '#9ecae1'}
+      case 3:
+        return {backgroundColor: '#3182bd'}
+      default:
+        return {backgroundColor: '#ffffff'}
+    }
+  }
+
+  render() {
+    return (
+      <button 
+        onClick={this.handleClick} 
+        className="curri-button" 
+        style={this.buttonStyle(this.props.topic.value)}>
+        {this.props.text}
+      </button>
+    )
+  }
+}
+
+export default ToggleButton
